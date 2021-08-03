c41b64f2dac009b95a98ea0d56739d13d53802c6
Chris Haas <chris@vendiadvertising.com>
2021-08-03 17:05:05 -0500

    Working

--- page-parts/component/body_copy.php
+++ page-parts/component/body_copy.php
@@ -1,32 +1,43 @@
 <?php
 
+$headline = get_sub_field('headline');
 $editor_mode = get_sub_field('editor_mode');
 $columns = get_sub_field('columns');
 $body_copy = get_sub_field('body_copy');
+$call_to_action = get_sub_field('call_to_action');
 
 if (!in_array($editor_mode, ['textarea', 'wysiwyg'], true)) {
     return;
 }
 ?>
+<section class="section-body-copy">
+    <?php if ($headline): ?>
+        <h2><?php esc_html_e($headline); ?></h2>
+    <?php endif; ?>
+    <div class="body-copy region column-count-<?php esc_attr_e((int)$columns); ?>">
+        <?php
+        switch ($editor_mode) {
+            case 'textarea':
+                echo wp_kses(
+                    wpautop($body_copy),
+                    [
+                        'p' => [],
+                    ]
+                );
+                break;
 
-<div class="body-copy column-count-<?php esc_attr_e((int)$columns); ?>">
-    <?php
-    switch ($editor_mode) {
-        case 'textarea':
-            echo wp_kses(
-                wpautop($body_copy),
-                [
-                    'p' => [],
-                ]
-            );
-            break;
-
-        case 'wysiwyg':
-            echo wp_kses(
-                $body_copy,
-                wp_kses_allowed_html()
-            );
-            break;
-    }
-    ?>
-</div>
+            case 'wysiwyg':
+                echo wp_kses(
+                    $body_copy,
+                    wp_kses_allowed_html()
+                );
+                break;
+        }
+        ?>
+    </div>
+    <?php if ($call_to_action): ?>
+        <div class="cta-button">
+            <a href="<?php esc_attr_e($call_to_action['url']); ?>" class="button"><?php esc_html_e($call_to_action['title']); ?></a>
+        </div>
+    <?php endif; ?>
+</section>
--- page-parts/component/recipes.php
+++ page-parts/component/recipes.php
@@ -1,6 +1,25 @@
 <?php
 
-$recipes = get_sub_field('recipes');
+$header = get_sub_field('header');
+
+$recipes = [];
+switch (get_sub_field('display_mode')) {
+    case 'curated':
+        $recipes = get_sub_field('recipes');
+        break;
+    case 'most-recent':
+        $recipes = get_posts(
+            [
+                'post_type' => 'recipe',
+                'numberposts' => (int)get_sub_field('maximum_number_to_show'),
+            ]
+        );
+        break;
+    default:
+        return;
+}
+
+
 if (!is_array($recipes) || !count($recipes)) {
     return;
 }
@@ -10,29 +29,37 @@ $recipes = [$recipes[0], $recipes[0], $recipes[0], $recipes[0]];
 ?>
 
 <section class="component-recipes region">
-    <?php foreach ($recipes as $recipe): ?>
-        <div class="single-recipe">
-            <div class="recipe-image">
-                <?php echo vendi_fly_get_attachment_picture(get_post_thumbnail_id($recipe), 'recipe-grid'); ?>
+    <?php if ($header): ?>
+        <h2><?php esc_html_e($header); ?></h2>
+    <?php endif; ?>
+    <div class="recipes">
+        <?php foreach ($recipes as $recipe): ?>
+            <div class="single-recipe">
+                <div class="recipe-image">
+                    <?php echo vendi_fly_get_attachment_picture(get_post_thumbnail_id($recipe), 'recipe-grid'); ?>
+                </div>
+                <div class="recipe-details">
+                    <h3><?php esc_html_e(get_the_title($recipe)); ?></h3>
+                    <?php
+                    $ingredients = get_field('ingredients', $recipe);
+                    if ($ingredients):
+                        ?>
+                        <h4>Ingredients</h4>
+                        <ul>
+                            <?php foreach ($ingredients as $ingredient): ?>
+                                <li>
+                                    <span><?php esc_html_e($ingredient['ingredient']); ?></span>
+                                    <span>&bull;</span>
+                                    <span><?php esc_html_e($ingredient['amount']); ?></span>
+                                </li>
+                            <?php endforeach; ?>
+                        </ul>
+                    <?php endif; ?>
+                    <a href="<?php esc_attr_e(get_permalink($recipe)); ?>" class="button">
+                        View more
+                    </a>
+                </div>
             </div>
-            <div class="recipe-details">
-                <h3><?php esc_html_e(get_the_title($recipe)); ?></h3>
-                <?php
-                $ingredients = get_field('ingredients', $recipe);
-                if ($ingredients):
-                    ?>
-                    <h4>Ingredients</h4>
-                    <ul>
-                        <?php foreach ($ingredients as $ingredient): ?>
-                            <li>
-                                <span><?php esc_html_e($ingredient['ingredient']); ?></span>
-                                <span>&bull;</span>
-                                <span><?php esc_html_e($ingredient['quantity']); ?></span>
-                            </li>
-                        <?php endforeach; ?>
-                    </ul>
-                <?php endif; ?>
-            </div>
-        </div>
-    <?php endforeach; ?>
+        <?php endforeach; ?>
+    </div>
 </section>
